a5c78605516edfa5b159f5f30ede1501c46d0b48
Nimesh55 <80301441+Nimesh55@users.noreply.github.com>
2022-09-30 09:38:53 +0530

    Added draw_arc function

--- helpers/graphs.py
+++ helpers/graphs.py
@@ -5,30 +5,39 @@ from string import ascii_letters
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+from charts.arc_diagram import ArcDiagram
 from charts.sankey_chart import SankeyChart
 
 
-#function for drawing our sankey chart
+# function for drawing our sankey chart
 def draw_sankey(root):
-    sankeychart=SankeyChart("./csv_samples/sankey_sample.csv")
+    sankeychart = SankeyChart("./csv_samples/sankey_sample.csv")
     figure = sankeychart.generate_chart()
-   
+
     chart = FigureCanvasTkAgg(figure, root)
     return chart
-    #chart.get_tk_widget().grid(row=1, column=2,columnspan=2, rowspan=4, pady=2, padx=20, sticky="ns")
+    # chart.get_tk_widget().grid(row=1, column=2,columnspan=2, rowspan=4, pady=2, padx=20, sticky="ns")
 
+
+# function for drawing arc diagram
+def draw_arc(root):
+    arc_diagram = ArcDiagram("./csv_samples/arc_sample.csv", graph_colour="red")
+    figure = arc_diagram.generate_chart()
+
+    chart = FigureCanvasTkAgg(figure, root)
+    return chart
 
 
 def draw_simple_matplotlib_chart(root):
     x = np.linspace(0, 10, 100)
-    y = 4 + 2 * np.sin(2 * x)   
+    y = 4 + 2 * np.sin(2 * x)
 
-    figure = plt.figure(figsize = (3,2), dpi = 100)
-    a=figure.add_subplot(111).plot(x, y, linewidth=2.0)
-  
-    
+    figure = plt.figure(figsize=(3, 2), dpi=100)
+    a = figure.add_subplot(111).plot(x, y, linewidth=2.0)
+
     chart = FigureCanvasTkAgg(figure, root)
-    chart.get_tk_widget().grid(row=1, column=2,columnspan=2, rowspan=4, pady=2, padx=20, sticky="ns")
+    chart.get_tk_widget().grid(row=1, column=2, columnspan=2, rowspan=4, pady=2, padx=20, sticky="ns")
+
 
 def draw_simple_seaborn_chart(root):
     sns.set(style="white")
@@ -54,14 +63,15 @@ def draw_simple_seaborn_chart(root):
     # Draw the heatmap with the mask and correct aspect ratio
     sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
                 square=True, linewidths=.5, cbar_kws={"shrink": .5})
-    
+
     canvas = FigureCanvasTkAgg(f, master=root)  # A tk.DrawingArea.
     canvas.draw()
-    canvas.get_tk_widget().grid(row=1, column=2,columnspan=2, rowspan=4, pady=2, padx=20, sticky="ns")
+    canvas.get_tk_widget().grid(row=1, column=2, columnspan=2, rowspan=4, pady=2, padx=20, sticky="ns")
+
 
 def draw_iris_data(root):
     df = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
-    df_setosa = df.loc[df['species']=='setosa']
+    df_setosa = df.loc[df['species'] == 'setosa']
     df_virginica = df.loc[df['species'] == 'virginica']
     df_versicolor = df.loc[df['species'] == 'versicolor']
 
@@ -72,8 +82,7 @@ def draw_iris_data(root):
     plt.plot(df_virginica['sepal_length'], np.zeros_like(df_virginica['sepal_length']), 'D')
     plt.plot(df_versicolor['sepal_length'], np.zeros_like(df_versicolor['sepal_length']), 'D')
     plt.xlabel('Petal length')
-    
+
     canvas = FigureCanvasTkAgg(f, master=root)  # A tk.DrawingArea.
     canvas.draw()
-    canvas.get_tk_widget().grid(row=1, column=2,columnspan=2, rowspan=4, pady=2, padx=20, sticky="ns")
-
+    canvas.get_tk_widget().grid(row=1, column=2, columnspan=2, rowspan=4, pady=2, padx=20, sticky="ns")
